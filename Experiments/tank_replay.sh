915b5c3c3cf7053396f8ec7589065e782890bf26
William <weilunwc@andrew.cmu.edu>
2018-06-07 18:34:01 +0900

    added harbor replay

--- Experiments/tank_replay.sh
+++ Experiments/tank_replay.sh
@@ -3,4 +3,4 @@ b="/tank_test.bag"
 bagfile="$a$b"
 
 water_depth=`cat tank_test.yaml`
-roslaunch underwater_experiments tank_experiment_replay.launch bagfile:=$bagfile 
+roslaunch underwater_experiments tank_experiment_replay.launch bagfile:=$bagfile water_depth:=$water_depth
